ece44c8dd830d429b47ad09ef732792f3520cd10
Vaishnav <vaishnavvaichu123@gmail.com>
2023-12-04 13:12:40 +0530

    upload and search

--- main.py
+++ main.py
@@ -7,25 +7,7 @@ import weaviate
 client = weaviate.connect_to_local()
 collection_name = "main"
 
-# Create a collection with multi2vec-clip as the vectorizer
-# collection = client.collections.create(
-#     name=collection_name,
-#     vectorizer_config=wvc.Configure.Vectorizer.multi2vec_clip(["image"], ["description"]),
-#     properties=[
-#         wvc.Property(
-#             name="image",
-#             data_type=wvc.DataType.BLOB
-#         ),
-#         wvc.Property(
-#             name="description",
-#             data_type=wvc.DataType.TEXT
-#         ),
-#         wvc.Property(
-#             name="category",
-#             data_type=wvc.DataType.TEXT
-#         )
-#     ]
-# )
+
 
 while True:
     url = input("Enter image URL (leave empty to exit): ")
@@ -36,8 +18,14 @@ while True:
     category = input("Enter category: ")
 
     # Download the image
-    response = requests.get(url)
-    image_data = BytesIO(response.content)
+    # response = requests.get(url)
+
+    image_data = None
+    # Read the image
+    with open('media/page_1_image_1.jpg', 'rb') as file:
+        image_data = BytesIO(file.read())
+
+
 
     # Convert the image to base64
     encoded_image = base64.b64encode(image_data.read()).decode()
