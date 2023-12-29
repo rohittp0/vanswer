227a466e96343c498fa38f40b81cf507f25d9ef3
Don Xavier <donxavier369@gmail.com>
2023-12-29 23:40:41 +0530

    make changes for better UI

--- server/home/urls.py
+++ server/home/urls.py
@@ -4,7 +4,6 @@ from home import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('navbar', views.navbar, name='navbar'),
     path('search/', views.search, name='search'),
     path('organization/',views.organization, name='organization'),
     path('searchresult/', views.searchresult, name='searchresult'),
