046736c750cb9de15c3afa64c0afa454932c08cf
Vaishnav <vaishnavvaichu123@gmail.com>
2024-01-06 09:03:05 +0530

    UI changes

---
+++ server/home/templatetags/constant_tags.py
@@ -0,0 +1,25 @@
+# myapp/templatetags/my_tags.py
+
+from django import template
+from home.constants import language_choices, category_choices, state_choices
+
+register = template.Library()
+
+
+@register.simple_tag
+def get_categories():
+    # Define your list here
+    return category_choices
+
+
+@register.simple_tag
+def get_filters():
+    filters = {
+        "Language": ["language", language_choices],
+        "Category": ["format", category_choices],
+        # "file_type": [file_types],
+        "Location": ["location", state_choices],
+        # "url_type": [url_types],
+    }
+
+    return filters
